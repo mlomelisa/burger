ce7f2ae8f3911c25545dea7f96d59a573f484547
mlomelisa <monica.lomelim@gmail.com>
2019-08-23 23:19:53 -0700

    Created files structure

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+use burgers_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double Cheesse", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Burger", false);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("Vegan Burger", false);
+
